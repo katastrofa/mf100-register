1636583d1c9c82024ad0aec5886d230515f84f03
Peter Baran <katastrofa42@gmail.com>
2015-05-19 21:47:26 +0200

    Version bump to 0.3.0

--- mf100-register.php
+++ mf100-register.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: MF100 registration
 Description: Registration form and management options for MF100
-Version: 0.3.0-SNAPSHOT
+Version: 0.3.0
 Author: Peter Baran
 License: LGPL2
 */
